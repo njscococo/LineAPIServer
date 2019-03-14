38c2c16f6f35dfba7530f013ba6539448cb62d58
njscococo <njscococo@gmail.com>
2019-03-15 01:12:45 +0800

    fixed bug

--- query.js
+++ query.js
@@ -9,6 +9,7 @@ const pool = new Pool({
 
 const insertImage = (req, res) => {
     const { userId } = req.body;
+    console.log('UserID:', userId)
     pool.query('insert into lineimage ( userId) values ($1 )', [userId], (err, results) => {
         if (err) {
             throw err;
