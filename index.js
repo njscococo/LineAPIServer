4628f5bd6a02a83351fc90e0d7c40fa2f439a351
njscococo <njscococo@gmail.com>
2019-03-15 00:04:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+const db = require('./query');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/',(req, res)=>{
+  res.json({info: 'hihi'})
+});
+app.post('/users', db.insertImage);
+
+app.listen(port,()=>{
+  console.log('app is working')
+});
---
+++ query.js
@@ -0,0 +1,29 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'ubxeypnqtxjfat',
+    host: 'ec2-50-19-109-120.compute-1.amazonaws.com',
+    database: 'dddbu83hpf94v5',
+    password: 'e82a641952012059ce136a382771f31b8d0c9011087864d59dbff074f570c804',
+    port: 5432
+});
+
+const insertImage = (req, res)=>{
+    const {image, userId} = req.body;
+    pool.query('insert into lineimage (drawImage, userId) values ($1, $2)',[image, userId], (err, results)=>{
+        if(err){
+            throw err;
+        }
+        console.log('result:', results);
+        res.status(201).send('image added with userId:')
+
+    }
+
+    )
+
+}
+
+
+module.exports = {
+    insertImage
+
+}
