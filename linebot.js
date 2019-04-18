497432cd67f7ec3524a3907690c3e9b098b2c77e
voyagerlin <voyagerlin@gmail.com>
2019-04-19 06:50:37 +0800

    first commit code

---
+++ linebot.js
@@ -0,0 +1,22 @@
+const linebot = require('linebot');
+
+let bot = linebot({
+    channelId: process.env.LINE_CHANNELID,
+    channelSecret: process.env.LINE_CHANNELSECRET,
+    channelAccessToken: process.env.LINE_CHANNELACCESSTOKEN
+  });
+
+console.log('linebot config:', process.env.LINE_CHANNELID)
+bot.on('message', evt=>{
+    console.log('recieve :', evt)
+    var replyMsg = `hihi, you said:${evt.message.text}`;
+    evt.reply(replyMsg).then(data=>{
+        console.log('send msg done:', data);
+    }).catch(err=>{
+        console.log('err:', err);
+    })
+})
+
+module.exports = {
+    bot
+}
---
+++ query.js
@@ -0,0 +1,130 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'ubxeypnqtxjfat',
+    host: 'ec2-50-19-109-120.compute-1.amazonaws.com',
+    database: 'dddbu83hpf94v5',
+    password: 'e82a641952012059ce136a382771f31b8d0c9011087864d59dbff074f570c804',
+    port: 5432,
+    ssl: true
+});
+
+const insertImage = (req, res) => {
+    const { userId, drawImage } = req.body;
+    //console.log('UserID:', userId)
+    pool.query('insert into lineimage ( "userid","image") values ($1 , $2) returning id', [userId, drawImage], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        //console.log('result userId:', userId, results);
+        res.status(201).json(results.rows[0])
+
+    }
+    )    
+}
+
+const queryProductImageById = (req, res) => {
+    const prodId = req.params.prodId;
+    pool.query('select image from products where "id"= $1 ', [prodId], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        
+        var img =  Buffer.from(results.rows[0].image.split(',')[1], 'base64');
+
+        res.writeHead(200, {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': img.length
+        });
+        res.end(img);
+
+    })
+}
+
+const queryImageById = (req, res) => {
+    const userId = req.params.userId;
+    const id = req.params.id;
+    console.log('queryImage:', userId, id)
+    pool.query('select "image" from lineimage where "userid"= $1 and id=$2 order by id desc limit 1', [userId, id], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        
+        var img =  Buffer.from(results.rows[0].image.split(',')[1], 'base64');
+
+        res.writeHead(200, {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': img.length
+        });
+        res.end(img);
+
+    })
+}
+
+const queryAllLineId = (req, res) =>{
+    pool.query('select email, lineuserid from users where lineuserid is not null',(err, result)=>{
+        if(err){
+            throw err;
+        }
+        res.status(201).json(result.rows)
+    })
+}
+
+const queryIsTmnewa = (req, res)=>{
+    const userId = req.params.userId;
+    console.log(req.params)
+    checkDBIsTmnewa(userId).then(result=>{
+        res.json({'isTmnewa': result})
+    })
+}
+
+const checkDBIsTmnewa = (lineUserId) => {
+    return new Promise((resolve, reject) => {
+        pool.query('select count(*) from users where lineuserid=$1', [lineUserId], (err, result)=>{
+            if(err){
+                reject(err) ;
+                return;
+            }
+            //console.log('queryIsTmnewa', '1' > 0);
+            resolve(result.rows[0].count > 0 ? true : false);
+        })
+    })
+    
+}
+
+const linkTmnewaAccount = (req, res)=>{
+    const {tmnewaid, userid} = req.body;
+    //console.log('linktmnewa', tmnewaid, userid)
+
+    pool.query('insert into users (email, lineuserid) values ($1, $2) returning id', [tmnewaid, userid], (err, result)=>{
+        if(err){
+            throw err;
+        }
+        //console.log(result)
+        res.status(201).json(result.rows[0])
+
+    })
+
+}
+
+const queryProducts = (req,res) => {
+    pool.query('select id, title, price from products limit 3', (err,result) => {
+        if (err) {
+            throw err;
+        }
+        //console.log('queryProducts', result)
+        res.status(201).json(result.rows)
+    })
+}
+
+
+
+module.exports = {
+    insertImage,
+    queryImageById,
+    queryIsTmnewa,
+    linkTmnewaAccount,
+    queryAllLineId,
+    queryProducts,
+    queryProductImageById,
+    checkDBIsTmnewa
+}
