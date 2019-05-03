a6800d52acb4f95c0bc4a0328f78e6228aac3235
njscococo <njscococo@gmail.com>
2019-05-03 17:16:20 +0800

    add userid binding

--- linebot.js
+++ linebot.js
@@ -1,4 +1,5 @@
 const linebot = require('linebot');
+const db = require('./query');
 
 let myLineBot = linebot({
     channelId: process.env.LINE_CHANNELID,
@@ -6,16 +7,81 @@ let myLineBot = linebot({
     channelAccessToken: process.env.LINE_CHANNELACCESSTOKEN
   });
 
-//console.log('linebot config:', process.env.LINE_CHANNELID)
-myLineBot.on('message', evt=>{
-    console.log('recieve :', evt)
-    var replyMsg = `hihi, you said:${evt.message.text}`;
-    evt.reply(replyMsg).then(data=>{
-        console.log('send msg done:', data);
-    }).catch(err=>{
-        console.log('err:', err);
-    })
-}) 
+myLineBot.on('message', function (event) {
+  console.log('bot message:', event.message);
+  //event.reply(event.message.text)
+  switch (event.message.type) {
+    case 'text':
+      if (event.message.text === '產品清單') {
+        //console.log('event.source.userId', event.source.userId)
+        db.linebot.checkDBIsTmnewa(event.source.userId).then(result => {                  
+          if (result) {
+            axios({
+              url: 'https://linetestingserver.herokuapp.com/products',
+              method: 'get'
+            })
+              .then((res) => {
+                //console.log('product', res.data)
+                let columns = res.data.map((elm, idx) => {
+                  return {
+                    title: elm.title,
+                    text: elm.price,
+                    actions: [{
+                      "type": "message",
+                      "label": "Yes",
+                      "text": "Yes"
+                    },
+                    {
+                      "type": "postback",
+                      "label": "Buy",
+                      "data": "action=buy&itemid=111",
+                      "text": "Buy"
+                    }
+                    ],
+                    thumbnailImageUrl: `https://linetestingserver.herokuapp.com/productimg/${elm.id}`
+                  }
+                })
+                event.reply({
+                  "type": "template",
+                  "altText": "this is a carousel template",
+                  "template": {
+                    "type": "carousel",
+                    "imageAspectRatio": "rectangle",
+                    "imageSize": "cover",
+                    "columns": columns
+                  }
+                })
+              })
+              .catch((err) => {
+
+              })
+
+          }else{
+            event.reply('請先綁定帳號')
+          }
+
+        })
+
+
+      }else if(event.message.text === '帳號綁定'){
+          db.linebot.queryIsLinked(event.source.userId)
+          .then((result) => {
+              console.log('id binding:', result)
+          })
+
+      } else {
+        event.reply(event.message.text)
+          .then(function (data) {
+            console.log('Success', data);
+          }).catch(function (error) {
+            console.log('Error', error);
+          });
+      }
+
+      break;
+
+  }
+});
 
 module.exports = {
     myLineBot
