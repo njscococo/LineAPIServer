0c64e8af40c9259636bc51052cfec34160632037
njscococo <njscococo@gmail.com>
2019-05-07 10:01:48 +0800

    merge many commits
    testing countdown

--- otp.js
+++ otp.js
@@ -13,7 +13,7 @@ redisClient.auth(process.env.REDIS_PW);
 
 const genOTP = function (tmnewaid) {
     otplib.authenticator.options = {
-        step: 20,
+        step: 620,
         window: 1
     };
 
@@ -22,33 +22,58 @@ const genOTP = function (tmnewaid) {
         const token = otplib.authenticator.generate(secret);
 
         redisClient.SETEX(tmnewaid, 620, secret);
+        redisClient.set(`nonce-${tmnewaid}`, secret);
+        redisClient.set( secret, tmnewaid);
 
         resolve(token);
     })
 }
 
 const validateOTP = function (token, tmnewaid) {
-    let isValid = false;
-    redisClient.get(tmnewaid, (err, reply) => {
-        if(err){
-            console.log('redis get key err:', err);
-            throw err;
-        }
-        console.log('otp1:', reply, token);
-        isValid = otplib.authenticator.check(token, reply.toString());
-        console.log('otp2:', isValid, reply, token);
-        if(isValid){
-            redisClient.del(tmnewaid, (error, result) => {
-                if(error){
-                    console.log('redis del key err:', error);
-                    throw error;
-                }
-                console.log('redis del done:', result);
-            });
-        }
+    return new Promise((resolve, reject) => {
+        let isValid = false;
+        let result = {};
+        console.log('otp0:', token, tmnewaid);
+        redisClient.get(tmnewaid, (err, reply) => {
+            if (err) {
+                console.log('redis get key err:', err);
+                throw err;
+            }
+            console.log('otp1:', reply, token);
+            isValid = otplib.authenticator.check(token, reply);
+            console.log('otp2:', isValid, reply, token);
 
-        return isValid;
-    });
+            if (isValid) {
+                redisClient.del(tmnewaid, (error, result) => {
+                    if (error) {
+                        console.log('redis del key err:', error);
+                        throw error;
+                    }
+                    console.log('redis del done:', result);
+                });
+
+                //Get the nonce key from redis
+                redisClient.get(`nonce-${tmnewaid}`, (err, rep) => {
+                    if (err) {
+                        console.log('get nonce err:', err);
+                        throw err;
+                    }
+
+                    console.log('nonce key:', rep);
+
+                    result.isValid = isValid;
+                    result.nonce = rep;
+                    resolve( result);
+                })
+
+            } else {
+                result.isValid = isValid;
+                resolve( result);
+            }
+        });
+
+    })
+
 }
 
 module.exports = {
